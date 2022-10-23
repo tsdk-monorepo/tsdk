bc130f91f95f278f8ef987f1936c0e036f75f875
suhaotian <h4av.one@gmail.com>
2022-10-24 00:20:09 +0800

    fix web example

--- examples/web/pages/index.tsx
+++ examples/web/pages/index.tsx
@@ -1,42 +1,68 @@
 // import { HomeScreen, HomeIcon } from '@acme/feature-home';
+import axios from 'axios';
 import {
   setHandler,
   setSocketIOInstance,
   socketIOHandler,
   setAxiosInstance,
   axiosHandler,
+  getHandler,
 } from 'fe-sdk-demo';
+import { QueryTodoRes } from 'fe-sdk-demo/lib/apiconf-refs';
 import { QueryTodo } from 'fe-sdk-demo/lib/user-api';
-import axios from 'axios';
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 // eslint-disable-next-line import/namespace
 import { io as SocketIO } from 'socket.io-client';
 
 import styles from '../styles/Home.module.css';
 
-(() => {
-  const io = SocketIO('http://localhost:3012/', {
-    transports: ['websocket'],
-  });
-  setSocketIOInstance(io);
-
-  io.on('connect', async () => {
-    setHandler(socketIOHandler);
-
-    const wsRes = await QueryTodo({});
-
-    console.log(wsRes);
-    io.disconnect();
-
-    setAxiosInstance(axios.create({ baseURL: 'http://localhost:3012/api' }));
-    setHandler(axiosHandler);
-
-    const httpRes = await QueryTodo({});
-    console.log(httpRes);
-  });
-})();
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? '' //  @todo 使用环境变量打包
+    : (() => {
+        if (typeof window === 'undefined') return;
+        return (
+          window?.location.protocol + '//' + window?.location.host.split(':')[0] + ':' + 3012 + '/'
+        );
+      })();
+const socketURL = baseURL;
+const apiURL = baseURL + 'api';
 
 export default function Home() {
+  const [handlerName, setHanlderName] = useState('');
+  const [result, setResult] = useState<QueryTodoRes>();
+
+  useEffect(() => {
+    const io = SocketIO(socketURL, {
+      transports: ['websocket'],
+    });
+    setSocketIOInstance(io);
+    setHandler(socketIOHandler);
+
+    setHanlderName(getHandler().name);
+
+    io.on('connect', async () => {
+      const wsRes = await QueryTodo({});
+
+      console.log(wsRes);
+      setResult(wsRes);
+
+      io.disconnect();
+      setTimeout(async () => {
+        setAxiosInstance(axios.create({ baseURL: apiURL }));
+        setHandler(axiosHandler);
+
+        setHanlderName(getHandler().name);
+
+        const httpRes = await QueryTodo({});
+        console.log(httpRes);
+
+        setResult(httpRes);
+      }, 2500);
+    });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -45,7 +71,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>hello</main>
+      <main className={styles.main}>
+        Hello, {handlerName}: {JSON.stringify(result)}
+      </main>
     </div>
   );
 }
