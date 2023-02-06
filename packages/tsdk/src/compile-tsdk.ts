1568288b2348b213b17724b207c7ac96ddcba338
suhaotian <h4av.one@gmail.com>
2023-02-07 07:30:41 +0800

    try again..

--- packages/tsdk/src/compile-tsdk.ts
+++ packages/tsdk/src/compile-tsdk.ts
@@ -1,10 +1,15 @@
 import { execSync } from 'child_process';
+import glob = require('fast-glob');
+import path from 'path';
 
 import { ensureDir } from './config';
 
 export function buildSDK() {
   console.log(`buildSDK run: cd ${ensureDir} && npm run tsc:build`);
-  execSync(`cd ${ensureDir} && npm run tsc:build`, { stdio: 'inherit' });
+  glob([path.join(ensureDir, 'src/**/*.ts').replace(/\\/g, '/')]).then((files) => {
+    console.log(files);
+    execSync(`cd ${ensureDir} && npm run tsc:build`, { stdio: 'inherit' });
+  });
 }
 
 export function buildSDKDoc() {
