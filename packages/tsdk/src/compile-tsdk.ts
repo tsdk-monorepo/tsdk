aaa63ec39318bbc82cdf00d369cf5d752d765add
suhaotian <h4av.one@gmail.com>
2023-02-06 22:35:19 +0800

    add debug info

--- packages/tsdk/src/compile-tsdk.ts
+++ packages/tsdk/src/compile-tsdk.ts
@@ -3,6 +3,7 @@ import { execSync } from 'child_process';
 import { ensureDir } from './config';
 
 export function buildSDK() {
+  console.log(`buildSDK run: cd ${ensureDir} && npm run tsc:build`);
   execSync(`cd ${ensureDir} && npm run tsc:build`, { stdio: 'inherit' });
 }
 
