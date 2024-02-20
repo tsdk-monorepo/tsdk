e285df8ba60e58af813942da60ffa0fe242752e8
suhaotian <h4av.one@gmail.com>
2024-02-20 14:40:47 +1100

    chore: bump xior to 0.0.5 and update website

--- examples/server/tsdk.config.js
+++ examples/server/tsdk.config.js
@@ -7,10 +7,12 @@ module.exports = {
   entityLibName: ['typeorm', 'kysely'],
   entityExt: 'entity',
   apiconfExt: 'apiconf',
+  httpLib: 'xior',
   sharedDirs: ['./src/shared', './src/i18n'],
   dataHookLib: 'ReactQuery',
   dependencies: {
     i18next: '^23.7.13',
     'intl-pluralrules': '^2.0.1',
+    xior: '0.0.5',
   },
 };
