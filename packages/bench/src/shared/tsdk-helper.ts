d1b5e8748b7826d169d2fe26531ab796468bb509
suhaotian <h4av.one@gmail.com>
2024-03-25 13:00:22 +1100

    revert: delete should has body

--- packages/bench/src/shared/tsdk-helper.ts
+++ packages/bench/src/shared/tsdk-helper.ts
@@ -8,7 +8,6 @@ export const hasBodyMethods: { [key: string]: boolean | undefined } = {
   post: true,
   put: true,
   patch: true,
-  delete: true,
 };
 
 export function checkMethodHasBody(method: string) {
