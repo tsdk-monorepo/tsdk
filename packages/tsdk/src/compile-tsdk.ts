1267741ed4e86e4e2af77f8dad4ccb36b36c5d9f
suhaotian <h4av.one@gmail.com>
2025-03-01 15:11:38 +1100

    Fix actions

--- packages/tsdk/src/compile-tsdk.ts
+++ packages/tsdk/src/compile-tsdk.ts
@@ -1,16 +1,30 @@
 import { execSync } from 'child_process';
-
+import fsExtra from 'fs-extra';
 import { ensureDir } from './config';
 import { getNpmCommand } from './get-pkg-manager';
 
 export async function buildConfigs(needInstall = false) {
   const CMDs = getNpmCommand(process.cwd());
-  execSync(
-    `cd ${ensureDir} ${needInstall ? `&& ${CMDs.installCmd} ` : ``}&& ${CMDs.runCmd} tsc:build:cjs`,
-    {
+  if (needInstall) {
+    const cmd = `${CMDs.installCmd}`;
+    console.log(`Run \`${cmd}\` in dir: ${ensureDir}`);
+    execSync(cmd, {
+      cwd: ensureDir,
       stdio: 'inherit',
+    });
+    const isNodeModulesExists = await fsExtra.exists(`${ensureDir}/node_modules`);
+    if (!isNodeModulesExists) {
+      console.log(`\nRun \`npm install\``);
+      execSync(`npm install`, {
+        cwd: ensureDir,
+        stdio: 'inherit',
+      });
     }
-  );
+    execSync(`${CMDs.runCmd} tsc:build:cjs`, {
+      cwd: ensureDir,
+      stdio: 'inherit',
+    });
+  }
 }
 
 export async function buildSDK(needInstall = false) {
